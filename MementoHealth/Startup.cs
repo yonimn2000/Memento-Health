fd04358c5280fcbe2916ea95aafe8e95a28fb398
Yonatan Mankovich <yonimn2000@gmail.com>
2021-09-08 10:48:35 -0400

    Add project files.

---
+++ MementoHealth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MementoHealth.Startup))]
+namespace MementoHealth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
