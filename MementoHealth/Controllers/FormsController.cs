1426f66010b0106015c03ec91f58367efcc26f9e
wbuerkle <56562365+wbuerkle@users.noreply.github.com>
2021-10-01 21:09:29 -0400

    FormControllerAdded
    Added form controller and form model view.

---
+++ MementoHealth/Controllers/FormsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MementoHealth.Entities;
+using MementoHealth.Models;
+
+namespace MementoHealth.Controllers
+{
+    public class FormsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Forms
+        public ActionResult Index()
+        {
+            return View(db.Forms.OrderBy(u => u.Name).ToList().Select(u => new FormViewModel
+            {
+                FormId = u.FormId,
+                Name = u.Name,
+            }).ToList());
+        }
+
+        // GET: Forms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form);
+        }
+
+        // GET: Forms/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProviderId = new SelectList(db.Providers, "ProviderId", "Name");
+            return View();
+        }
+
+        // POST: Forms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FormId,Name,ProviderId")] Form form)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Forms.Add(form);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProviderId = new SelectList(db.Providers, "ProviderId", "Name", form.ProviderId);
+            return View(form);
+        }
+
+        // GET: Forms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProviderId = new SelectList(db.Providers, "ProviderId", "Name", form.ProviderId);
+            return View(GetEditFormViewModel(form));
+        }
+
+        private static EditFormViewModel GetEditFormViewModel(Form form)
+        {
+            return new EditFormViewModel
+            {
+                FormId = form.FormId,
+                Name = form.Name
+            };
+        }
+
+        // POST: Forms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(EditFormViewModel model)
+        {
+            Form form = db.Forms.Find(model.FormId);
+            if (ModelState.IsValid)
+            {
+                form.Name = model.Name;
+                db.SaveChanges();
+                TempData.Add("StatusMessage", "Form edited successfully.");
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProviderId = new SelectList(db.Providers, "ProviderId", "Name", form.ProviderId);
+            return View(GetEditFormViewModel(form));
+        }
+
+        // GET: Forms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Form form = db.Forms.Find(id);
+            if (form == null)
+            {
+                return HttpNotFound();
+            }
+            return View(form);
+        }
+
+        // POST: Forms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Form form = db.Forms.Find(id);
+            db.Forms.Remove(form);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Clone()
+        {
+            ViewBag.Message = "Are you sure you wish to clone this form?";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
