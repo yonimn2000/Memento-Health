4d8e0fea36f096319924ef5cdcbe354c36fbf296
Yonatan Mankovich <yonimn2000@gmail.com>
2021-10-04 16:48:22 -0400

    Restrict Forms controller.

--- MementoHealth/Controllers/FormsController.cs
+++ MementoHealth/Controllers/FormsController.cs
@@ -1,49 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web;
-using System.Web.Mvc;
+﻿using MementoHealth.Classes;
 using MementoHealth.Entities;
 using MementoHealth.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
 
 namespace MementoHealth.Controllers
 {
+    [Authorize(Roles = Role.ProviderAdmin + "," + Role.Doctor + "," + Role.Assistant)]
     public class FormsController : Controller
     {
-        private ApplicationDbContext db = new ApplicationDbContext();
+        private ApplicationDbContext Db { get; } = new ApplicationDbContext();
 
         // GET: Forms
         public ActionResult Index()
         {
-            return View(db.Forms.OrderBy(u => u.Name).ToList().Select(u => new FormViewModel
-            {
-                FormId = u.FormId,
-                Name = u.Name,
-            }).ToList());
+            return View(GetCurrentUserProvider().Forms.OrderBy(u => u.Name).ToList()
+                .Select(u => new FormViewModel
+                {
+                    FormId = u.FormId,
+                    Name = u.Name,
+                }).ToList());
         }
 
         // GET: Forms/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Form form = db.Forms.Find(id);
+
+            Form form = FindForm_Restricted(id);
             if (form == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(form);
         }
 
+        private Form FindForm_Restricted(int? id)
+        {
+            string userId = User.Identity.GetUserId();
+            return Db.Users.Find(userId).Provider.Forms.Where(f => f.FormId == id).SingleOrDefault();
+        }
+
         // GET: Forms/Create
+        [Authorize(Roles = Role.ProviderAdmin)]
         public ActionResult Create()
         {
-            ViewBag.ProviderId = new SelectList(db.Providers, "ProviderId", "Name");
             return View();
         }
 
@@ -52,32 +57,42 @@ namespace MementoHealth.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "FormId,Name,ProviderId")] Form form)
+        [Authorize(Roles = Role.ProviderAdmin)]
+        public ActionResult Create([Bind(Include = "Name")] Form form)
         {
             if (ModelState.IsValid)
             {
-                db.Forms.Add(form);
-                db.SaveChanges();
+                Form newForm = new Form
+                {
+                    Name = form.Name,
+                    ProviderId = GetCurrentUserProvider().ProviderId
+                };
+                Db.Forms.Add(newForm);
+                Db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.ProviderId = new SelectList(db.Providers, "ProviderId", "Name", form.ProviderId);
             return View(form);
         }
 
+        private Provider GetCurrentUserProvider()
+        {
+            if (User.IsInRole(Role.SysAdmin))
+                return null;
+            return Db.Users.Find(User.Identity.GetUserId()).Provider;
+        }
+
         // GET: Forms/Edit/5
+        [Authorize(Roles = Role.ProviderAdmin)]
         public ActionResult Edit(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Form form = db.Forms.Find(id);
+
+            Form form = FindForm_Restricted(id);
             if (form == null)
-            {
                 return HttpNotFound();
-            }
-            ViewBag.ProviderId = new SelectList(db.Providers, "ProviderId", "Name", form.ProviderId);
+
             return View(GetEditFormViewModel(form));
         }
 
@@ -95,78 +110,77 @@ namespace MementoHealth.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = Role.ProviderAdmin)]
         public ActionResult Edit(EditFormViewModel model)
         {
-            Form form = db.Forms.Find(model.FormId);
+            Form form = FindForm_Restricted(model.FormId);
             if (ModelState.IsValid)
             {
                 form.Name = model.Name;
-                db.SaveChanges();
-                TempData.Add("StatusMessage", "Form edited successfully.");
+                Db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.ProviderId = new SelectList(db.Providers, "ProviderId", "Name", form.ProviderId);
             return View(GetEditFormViewModel(form));
         }
 
         // GET: Forms/Delete/5
+        [Authorize(Roles = Role.ProviderAdmin)]
         public ActionResult Delete(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Form form = db.Forms.Find(id);
+
+            Form form = FindForm_Restricted(id);
             if (form == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(form);
         }
 
         // POST: Forms/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = Role.ProviderAdmin)]
         public ActionResult DeleteConfirmed(int id)
         {
-            Form form = db.Forms.Find(id);
-            db.Forms.Remove(form);
-            db.SaveChanges();
+            Form form = FindForm_Restricted(id);
+            Db.Forms.Remove(form);
+            Db.SaveChanges();
             return RedirectToAction("Index");
         }
 
         // GET: Forms/Clone/5
+        [Authorize(Roles = Role.ProviderAdmin)]
         public ActionResult Clone(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Form form = db.Forms.Find(id);
+
+            Form form = FindForm_Restricted(id);
             if (form == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(form);
         }
 
-        
+
         // POST: Forms/Delete/5
         [HttpPost, ActionName("Clone")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = Role.ProviderAdmin)]
         public ActionResult CloneConfirmed(int id)
         {
-            /*Form form = db.Forms.Find(id);
-            // TODO: Implement
-            db.SaveChanges();*/
             throw new NotImplementedException();
-            return RedirectToAction("Index");
+            /*Form form = FindForm_Restricted(id);
+            // TODO: Implement
+            db.SaveChanges();
+            return RedirectToAction("Index");*/
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
-                db.Dispose();
+                Db.Dispose();
 
             base.Dispose(disposing);
         }
