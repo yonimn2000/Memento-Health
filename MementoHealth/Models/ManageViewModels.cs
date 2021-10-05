05c88f49aad12c8865aa94adfb74356e39725d45
SBarya27 <59980035+SBarya27@users.noreply.github.com>
2021-10-05 18:39:23 -0400

    Updated "Users" and "Manage" Views

--- MementoHealth/Models/ManageViewModels.cs
+++ MementoHealth/Models/ManageViewModels.cs
@@ -34,7 +34,7 @@ namespace MementoHealth.Models
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
+        [Display(Name = "Confirm password")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
@@ -53,7 +53,7 @@ namespace MementoHealth.Models
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
+        [Display(Name = "Confirm password")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
@@ -72,7 +72,7 @@ namespace MementoHealth.Models
         public string NewPin { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new PIN")]
+        [Display(Name = "Confirm PIN")]
         [Compare("NewPin", ErrorMessage = "The new PIN and confirmation PIN do not match.")]
         public string ConfirmPin { get; set; }
     }
