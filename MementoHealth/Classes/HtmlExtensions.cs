ac7b26cd79c387b2cc8284d602f447454de1b406
Yonatan Mankovich <yonimn2000@gmail.com>
2021-10-18 10:25:07 -0400

    Create HtmlExtensions.cs

---
+++ MementoHealth/Classes/HtmlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace MementoHealth.Classes
+{
+    public static class HtmlExtensions
+    {
+        public static string Phone(this HtmlHelper helper, string phone)
+        {
+            return $"<a href='tel:{phone}'>{phone}</a>";
+        }
+
+        public static string Email(this HtmlHelper helper, string email)
+        {
+            return $"<a href='mailto:{email}'>{email}</a>";
+        }
+    }
+}
