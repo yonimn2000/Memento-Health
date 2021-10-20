8f55e1b73ce276acd33951c3cf0667d3cdeef9e2
Yonatan Mankovich <yonimn2000@gmail.com>
2021-10-20 18:33:54 -0400

    Graph bug fixes.

--- MementoHealth/Classes/StringExtensions.cs
+++ MementoHealth/Classes/StringExtensions.cs
@@ -5,6 +5,7 @@ namespace MementoHealth.Classes
     public static class StringExtensions
     {
         public static string Truncate(this string str, int maxLength) 
-            => string.IsNullOrEmpty(str) ? str : (str.Substring(0, Math.Min(str.Length, Math.Max(0, maxLength - 3))) + "...");
+            => string.IsNullOrEmpty(str) || str.Length <= maxLength ? str
+                : str.Substring(0, Math.Min(str.Length, Math.Max(0, maxLength - 3))) + "...";
     }
 }
