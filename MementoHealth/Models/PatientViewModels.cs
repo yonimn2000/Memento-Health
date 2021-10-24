4eab10ae55577e6ec07722ff07ab8e8efef4efcb
Yonatan Mankovich <yonimn2000@gmail.com>
2021-10-24 10:51:40 -0400

    Improve patient search.

--- MementoHealth/Models/PatientViewModels.cs
+++ MementoHealth/Models/PatientViewModels.cs
@@ -1,19 +1,21 @@
-﻿using System;
+﻿using MementoHealth.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MementoHealth.Models
 {
     public class PatientSearchModel
     {
-        [DisplayName("Full Name")]
+        [DisplayName("Full Name Contains")]
         public string FullName { get; set; }
 
+        [DisplayName("Birthday Equals")]
         public DateTime? Birthday { get; set; }
 
-        [DisplayName("External Id")]
+        [DisplayName("External Id Contains")]
         public string ExternalPatientId { get; set; }
+
+        public IEnumerable<Patient> Results { get; set; }
     }
 }
