06bc12cccce9e8d223ff6133e0c9b3857569b54c
Camron Farida <cfarida@oakland.edu>
2021-10-31 12:59:20 -0400

    Added current email

--- MementoHealth/Models/ManageViewModels.cs
+++ MementoHealth/Models/ManageViewModels.cs
@@ -12,6 +12,7 @@ namespace MementoHealth.Models
         public IList<UserLoginInfo> Logins { get; set; }
         public string PhoneNumber { get; set; }
         public string FullName { get; set; }
+        public string Email { get; set; }
         public string Role { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
